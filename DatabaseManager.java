914392e3e91a5bc96584bd460c06331e286c7a52
Sawega <146950588+Regaz12@users.noreply.github.com>
2026-06-26 20:19:32 +0700

    Update DatabaseManager.java

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 public class DatabaseManager {
     // Configuration for PostgreSQL as per user credentials
     private static final String URL = "jdbc:postgresql://localhost:5433/game_project";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
